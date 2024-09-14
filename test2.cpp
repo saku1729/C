e610aedfadb41b5366ada3e91131a9e15a2df2b1
saku1729 <sf24731s@st.omu.ac.jp>
2024-09-14 16:39:44 +0900

    a

--- test2.cpp
+++ test2.cpp
@@ -2,11 +2,5 @@
 using namespace std;
 int main()
 {
-    int b[5];
-    for (int i = 0; i < 5; i++)
-    {
-        cout << &b[i] << endl;
-    }
-    cout << &b << endl;
-    return 0;
+    cout << "Hello World" << endl;
 }
