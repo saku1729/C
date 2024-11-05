44728b3160ca801239361a6a49b54b69208aecb5
saku1729 <sf24731s@st.omu.ac.jp>
2024-11-05 12:23:13 +0900

    test

--- throw.c
+++ throw.c
@@ -3,7 +3,15 @@
 #include <math.h>
 #define g 9.8
 #define PI 3.141592
-int main()
+int main(void)
 {
+    double v, theta, t, L;
+    printf("初速度v[m/s]を入力してください。: ");
+    scanf("%lf", &v);
+    printf("角度θ[度]を入力してください。: ");
+    scanf("%lf", &theta);
+    double rad = theta * PI / 180;
+    L = v * v * sin(2 * rad) / g;
+    printf("距離L[m]は%lfです。\n", L);
     return 0;
 }
